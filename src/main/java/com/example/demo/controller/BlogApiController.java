de84917621af2f41fb4438ac5030496749766f55
suhpark <suhpark@gmarket.com>
2023-10-18 15:42:37 +0900

    6.4 블로그 글 목록 조회를 위한 API 구현하기

--- src/main/java/com/example/demo/controller/BlogApiController.java
+++ src/main/java/com/example/demo/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Article;
 import com.example.demo.dto.AddArticleRequest;
+import com.example.demo.dto.ArticleResponse;
 import com.example.demo.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +26,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
