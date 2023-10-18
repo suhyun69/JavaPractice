9e8823d960462b3e79be892809ecbfd3fcc54c8a
suhpark <suhpark@gmarket.com>
2023-10-18 15:25:25 +0900

    6.3 블로그 글 작성을 위한 API 구현하기

---
+++ src/main/java/com/example/demo/service/BlogService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Article;
+import com.example.demo.dto.AddArticleRequest;
+import com.example.demo.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
