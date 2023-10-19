e80164e4c83073b10f5f4a9f6ce279aa084417ac
suhpark <suhpark@gmarket.com>
2023-10-19 17:16:53 +0900

    SpringSecurity 적용 : password 암호화

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.AddUserRequest;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
