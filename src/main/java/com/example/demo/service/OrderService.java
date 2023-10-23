fe3b78da9853e809d18a0ecaaad122a4184fe953
suhpark <suhpark@gmarket.com>
2023-10-23 17:17:37 +0900

    JPA : Repository 및 Service 추가

---
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.*;
+import com.example.demo.domain.item.Item;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.OrderRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class OrderService {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired OrderRepository orderRepository;
+    @Autowired ItemService itemService;
+
+    /** 주문 */
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemService.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery(member.getAddress());
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+
+    /** 주문 취소 */
+    public void cancelOrder(Long orderId) {
+
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        //주문 취소
+        order.cancel();
+    }
+
+    /** 주문 검색 */
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
+
+}
